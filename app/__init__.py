a229049f47f99d5c18412baf28982e284f894980
marielaxcruz <marielacrtzcruz@gmail.com>
2021-04-30 09:47:11 -0700

    migrations

--- app/__init__.py
+++ app/__init__.py
@@ -5,11 +5,15 @@ from flask_migrate import Migrate
 db = SQLAlchemy()
 migrate = Migrate()
 
+
+# postgresql+psycopg2://postgres:postgres@localhost:5432/ada_books_development
+
+
 def create_app(test_config=None):
     app = Flask(__name__)
-
+    # DB config
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/ada_books_development'
 
     # Import models here
     from app.models.book import Book
@@ -20,5 +24,6 @@ def create_app(test_config=None):
     # Register Blueprints here
     from .routes import hello_world_bp
     app.register_blueprint(hello_world_bp)
+    
 
     return app
